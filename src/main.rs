213f94cc8ee683bd3866ff1eabb3779dda074415
ZyKaiyZ <yoyo8117431@gmail.com>
2023-11-26 21:15:22 +0800

    Feat: Update GUI

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use std::{process::Command, io::Write};
-use std::io::{stdin, stdout};
+use std::{process::Command, io::{stdin, stdout, Write}};
+slint::include_modules!();
 
-fn get_user_input(prompt: &str) -> String {
+fn get_input(prompt: &str) -> String {
     print!("{}", prompt);
-    stdout().flush().unwrap();
+    stdout().flush().unwrap(); // flush it to the screen
 
     let mut input = String::new();
     stdin().read_line(&mut input).expect("Failed to read line");
@@ -17,19 +17,39 @@ fn clip_video(start_time: &str, end_time: &str, input_file: &str){
     let output = Command::new("powershell")
         .arg(command)
         .output()
-        .expect("failed to execute process");
+        .expect("Failed to execute process");
 
     println!("output: {:?}", String::from_utf8_lossy(&output.stdout));
 }
 
+fn ui(start_time: &str, end_time: &str, input_file: &str) -> Result<(), slint::PlatformError> {
+    let ui = MainWindow::new()?;
+
+    let ui_handle = ui.as_weak();
+    let ui = ui_handle.unwrap();
+    ui.set_start_time(start_time.into());
+    ui.set_end_time(end_time.into());
+    ui.set_input_file(input_file.into());
+
+    ui.on_clip_video(move || {
+        let ui_copy = ui_handle.unwrap();
+
+        clip_video(&ui_copy.get_start_time(), 
+                &ui_copy.get_end_time(), &ui_copy.get_input_file());
+    });
+
+    ui.run()
+}
+
 fn main() {
-    let start_time = get_user_input("Enter start time: ");
-    let end_time = get_user_input("Enter end time: ");
-    let input_file = get_user_input("Enter input file: ");
+    let start_time = get_input("Enter start time (format : HH:mm:ss): ");
+    let end_time = get_input("Enter end time (format : HH:mm:ss) : ");
+    let input_file = get_input("Enter input file (include .mp4) : ");
+
 
     if cfg!(target_os = "windows") {
         println!("This is windows");
-        clip_video(&start_time, &end_time, &input_file);
+        ui(&start_time, &end_time, &input_file).unwrap();
     } else if cfg!(target_os = "linux") {
         println!("This is linux");
         // todo
