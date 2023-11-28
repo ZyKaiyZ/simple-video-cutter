0a582bc61f7d94a85ef69c4330664cc8b4dc12a1
Kai Hong <Kai.Hong@wiadvance.com>
2023-11-28 16:26:15 +0800

    Feat: ComboBox for selecting file names and refactor UI module.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::process::Command;
-slint::include_modules!();
+
+mod ui;
+use ui::ui;
 
 fn is_ffmpeg_installed() -> bool {
     let output = Command::new("ffmpeg")
@@ -14,38 +16,12 @@ fn is_ffmpeg_installed() -> bool {
     }
 }
 
-fn clip_video(start_time: &str, end_time: &str, input_file: &str){
-    let output_file = input_file.replace(".mp4", "_clip.mp4");
-    let command = format!("ffmpeg -ss {} -to {} -i {} {}", start_time, end_time, input_file, output_file);
-    let output = Command::new("powershell")
-        .arg(command)
-        .output()
-        .expect("Failed to execute process");
-
-    println!("output: {:?}", String::from_utf8_lossy(&output.stdout));
-}
-
-fn ui() -> Result<(), slint::PlatformError> {
-    let ui = MainWindow::new()?;
-
-    let ui_handle = ui.as_weak();
-    let ui = ui_handle.unwrap();
-
-    ui.on_clip_video(move || {
-        let ui = ui_handle.unwrap();
-
-        clip_video(&ui.get_start_time(), &ui.get_end_time(), &ui.get_input_file());
-    });
-
-    ui.run()
-}
-
 fn main() {
     if cfg!(target_os = "windows") {
         println!("This is windows");
+        ui().unwrap();
 
         if is_ffmpeg_installed() {
-            ui().unwrap();
         } else {
             println!("Error: ffmpeg is not installed. Please install ffmpeg and try again.");
         }
@@ -53,7 +29,8 @@ fn main() {
         println!("This is linux");
         // todo
     } else if cfg!(target_os = "macos") {
-        println!("This is macos");
+        println
+        !("This is macos");
         // todo
     } else {
         println!("This is unknown");
---
+++ src/ui.rs
@@ -0,0 +1,57 @@
+use slint::{SharedString, ModelRc, VecModel};
+use std::{fs, rc::Rc, process::Command};
+slint::include_modules!();
+
+fn clip_video(start_time: &str, end_time: &str, input_file: &str){
+    let output_file = input_file.replace(".mp4", "_clip.mp4");
+    let command = format!("ffmpeg -ss {} -to {} -i {} {}", start_time, end_time, input_file, output_file);
+    let output = Command::new("powershell")
+        .arg(command)
+        .output()
+        .expect("Failed to execute process");
+
+    println!("output: {:?}", String::from_utf8_lossy(&output.stdout));
+}
+
+fn find_mp4_files(directory: &str) -> Result<Vec<String>, std::io::Error> {
+    let entries = fs::read_dir(directory)?;
+
+    let mp4_files: Vec<String> = entries
+        .filter_map(|entry| {
+            entry.ok().and_then(|e| {
+                let path = e.path();
+                let extension = path.extension()?.to_string_lossy().to_lowercase();
+                if extension == "mp4" {
+                    Some(path.file_stem()?.to_string_lossy().to_string())
+                } else {
+                    None
+                }
+            })
+        })
+        .collect();
+
+    Ok(mp4_files)
+}
+
+pub fn ui() -> Result<(), slint::PlatformError> {
+    let ui = MainWindow::new()?;
+
+    let ui_handle = ui.as_weak();
+    let ui = ui_handle.unwrap();
+
+    let mp4_files = find_mp4_files(".").unwrap();
+
+    let shared_mp4_files = mp4_files.into_iter().map(|s| SharedString::from(s)).collect::<Vec<SharedString>>();
+
+    let the_model = ModelRc::new(Rc::new(VecModel::from(shared_mp4_files)).clone());
+
+    ui.set_file_list(the_model);
+
+    ui.on_clip_video(move || {
+        let ui = ui_handle.unwrap();
+
+        clip_video(&ui.get_start_time(), &ui.get_end_time(), &ui.get_input_file());
+    });
+
+    ui.run()
+}
